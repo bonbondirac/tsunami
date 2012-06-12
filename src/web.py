3c3bb23ee8b8fe013a9a5f215a351c300dfa4c32
bonbondirac <diracfangustc@gmail.com>
2012-06-12 18:24:59 +0800

    add test code

---
+++ src/web.py
@@ -0,0 +1,19 @@
+'''
+Created on 2012-6-12
+
+@author: diracfang
+'''
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
