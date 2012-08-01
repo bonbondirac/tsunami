b04d693f126f1ff6229dd7cd0358f1753ee9bbac
bonbondirac <diracfangustc@gmail.com>
2012-08-01 13:24:18 +0800

    add InterruptHandler

--- src/web.py
+++ src/web.py
@@ -9,22 +9,33 @@ import tornado.web
 import time
 
 class MainHandler(tornado.web.RequestHandler):
+    cache = None
+    
     @tornado.web.asynchronous
     def get(self):
         self.infinite_loop(0)
         
     def infinite_loop(self, counter):
+        cls = MainHandler
+        if cls.cache:
+            self.write('interrupted on %s<br />\n' % cls.cache)
+            cls.cache = None
         self.write("Hello world! counter: %d<br />\n" % counter)
         self.flush()
         tornado.ioloop.IOLoop.instance().add_timeout(time.time() + 1,
                                                      lambda: self.infinite_loop(counter + 1))
+
+
+class InterruptHandler(tornado.web.RequestHandler):
     
-    def on_connection_close(self):
-        self.finish()
-    
+    def get(self):
+        cls = MainHandler
+        cls.cache = str(time.time())
+        
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/interrupt", InterruptHandler),
 ])
 
 if __name__ == "__main__":
