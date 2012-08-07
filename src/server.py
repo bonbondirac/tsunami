e23785e6dbc2ae0a2eca74ab1aeb052fccffe8d7
bonbondirac <diracfangustc@gmail.com>
2012-08-07 18:54:26 +0800

    start server code

---
+++ src/server.py
@@ -0,0 +1,20 @@
+'''
+Created on 2012-8-7
+
+@author: diracfang
+'''
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
