42e7a7e6d05d94490a1c03fd08d8b91e04593e98
bonbondirac <diracfangustc@gmail.com>
2012-08-09 16:28:18 +0800

    mod for debug

--- src/auth.py
+++ src/auth.py
@@ -11,21 +11,23 @@ class User(object):
         self.access_token = access_token
     
     def get_user(self):
-        if not hasattr(self, '_user'):
-            qs = "select * from account_access where access_token = '%s'" % self.access_token
-            result = self.db.get(qs)
-            if result:
-                self._user = result
-            else:
-                self._user = None
-        
-        return self._user
+#        if not hasattr(self, '_user'):
+#            qs = "select * from account_access where access_token = '%s'" % self.access_token
+#            result = self.db.get(qs)
+#            if result:
+#                self._user = result
+#            else:
+#                self._user = None
+#        
+#        return self._user
+        return 'me'
     
     def get_user_id(self):
-        user = self.get_user()
-        if user:
-            user_id = user.user_id
-        else:
-            user_id = None
-        
-        return user_id
+#        user = self.get_user()
+#        if user:
+#            user_id = user.user_id
+#        else:
+#            user_id = None
+#        
+#        return user_id
+        return 2
--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,6 @@ Created on 2012-8-8
 @author: diracfang
 '''
 KEY_USER_CHANNEL = 'user.channel:%s-%s' # require env_tag, user_id
-BREAK_SYMBOL = '\r\n'
-KEEP_ALIVE_INTERVAL = 30
+BREAK_SYMBOL = 'br<br />\r\n'
+KEEP_ALIVE_INTERVAL = 5
 DEFAULT_CHANNEL = 'default.channel'
