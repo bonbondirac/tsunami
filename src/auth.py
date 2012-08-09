85d99c9bb1fe4509f10fb5e1695bdf1a7bf7b945
bonbondirac <diracfangustc@gmail.com>
2012-08-09 19:22:52 +0800

    revert to normal config

--- src/auth.py
+++ src/auth.py
@@ -11,23 +11,21 @@ class User(object):
         self.access_token = access_token
     
     def get_user(self):
-#        if not hasattr(self, '_user'):
-#            qs = "select * from account_access where access_token = '%s'" % self.access_token
-#            result = self.db.get(qs)
-#            if result:
-#                self._user = result
-#            else:
-#                self._user = None
-#        
-#        return self._user
-        return 'me'
+        if not hasattr(self, '_user'):
+            qs = "select * from account_access where access_token = '%s'" % self.access_token
+            result = self.db.get(qs)
+            if result:
+                self._user = result
+            else:
+                self._user = None
+        
+        return self._user
     
     def get_user_id(self):
-#        user = self.get_user()
-#        if user:
-#            user_id = user.user_id
-#        else:
-#            user_id = None
-#        
-#        return user_id
-        return 2
+        user = self.get_user()
+        if user:
+            user_id = user.user_id
+        else:
+            user_id = None
+        
+        return user_id
