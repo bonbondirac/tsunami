8b0ff4e87aba5d3d03b97c5ef41bda59050c08c5
bonbondirac <diracfangustc@gmail.com>
2012-08-10 13:41:45 +0800

    revert to production constants

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,8 @@ Created on 2012-8-8
 @author: diracfang
 '''
 KEY_USER_CHANNEL = 'user.channel:%s-%s' # require env_tag, user_id
-BREAK_SYMBOL = 'br<br />\r\n'
-KEEP_ALIVE_INTERVAL = 10
+BREAK_SYMBOL = '\r\n'
+KEEP_ALIVE_INTERVAL = 60
 DEFAULT_CHANNEL = 'default.channel'
 
 msg_config = {
