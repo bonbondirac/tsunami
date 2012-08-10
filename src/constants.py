3d2b9068390a77263fd7916fa02af3e68a4cf97e
bonbondirac <diracfangustc@gmail.com>
2012-08-10 10:29:36 +0800

    modify consts

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,8 @@ Created on 2012-8-8
 @author: diracfang
 '''
 KEY_USER_CHANNEL = 'user.channel:%s-%s' # require env_tag, user_id
-BREAK_SYMBOL = '\r\n'
-KEEP_ALIVE_INTERVAL = 60
+BREAK_SYMBOL = 'br<br />\r\n'
+KEEP_ALIVE_INTERVAL = 10
 DEFAULT_CHANNEL = 'default.channel'
 
 msg_config = {
