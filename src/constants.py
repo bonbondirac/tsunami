56c45d657189bb9cc873677ac6b9136f57bd6dcd
bonbondirac <diracfangustc@gmail.com>
2012-08-10 16:29:27 +0800

    modify keep_alive interval to 30 due to nginx default settings

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,8 @@ Created on 2012-8-8
 '''
 KEY_USER_CHANNEL = 'user.channel:%s-%s' # require env_tag, user_id
 BREAK_SYMBOL = '\r\n'
-KEEP_ALIVE_INTERVAL = 60
+# due to nginx default settings, <60 should be your choice
+KEEP_ALIVE_INTERVAL = 30
 DEFAULT_CHANNEL = 'default.channel'
 
 msg_config = {
