c41943b12b94e78180b3bc87359ac2bf0e7749ef
bonbondirac <diracfangustc@gmail.com>
2012-10-30 10:57:23 +0800

    remove debug code

--- src/auth.py
+++ src/auth.py
@@ -7,10 +7,6 @@ Created on 2012-8-9
 import cPickle
 import settings
 import constants
-import time
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class User(object):
@@ -31,11 +27,7 @@ class User(object):
                     row = None
                 if row is None:
                     qs = "select * from account_access where access_token = %s"
-                    try:
-                        row = self.db.get(qs, self.access_token)
-                    except Exception, exc:
-                        logger.warning('idle time: %s' % (time.time() - self.db._last_use_time))
-                        raise exc
+                    row = self.db.get(qs, self.access_token)
                     if row:
                         self.redis_ins.set(key, cPickle.dumps(row))
                         self.redis_ins.expire(key, constants.ACCESS_TOKEN_CACHE_TIMEOUT)
